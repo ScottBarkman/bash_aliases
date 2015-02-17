784e55970aaf4630dcd701dda97006308c342da7
Scott Barkman <scott@liftinteractive.com>
2015-02-17 16:44:05 -0700

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,10 @@
 ##### Extract Function ######
-# $EDITOR=nano
 function extract {
  if [ -z "$1" ]; then
     # display usage if no parameters given
     echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
  else
     if [ -f $1 ] ; then
-        # NAME=${1%.*}
-        # mkdir $NAME && cd $NAME
         case $1 in
           *.tar.bz2)   tar xvjf ../$1    ;;
           *.tar.gz)    tar xvzf ../$1    ;;
@@ -31,29 +28,56 @@ function extract {
     fi
 fi
 }
+#######################################################################
+#
+# Find & generate import string for giving class name
+# ---------------------------------------------------------------------
+#
+# I got fed up seeing classes referenced in django docs, with no easy way
+# to find where they were imported from. (too much scrolling)
+# Simple recursive grep command to search through virtualenv site packages
+# folder for class you supply. For sake of speed, matching case is required
+# (camel case that suckah)
+#
+# Usage:
+#   imp ValidationError
+#
+#   NOTE: REQUIRES ACTIVE VIRTUALENV (uses sitepackages)
+#----------------------------------------------------------------------
 
+imp() {
+    cyan='\e[1;36m'
+    NC='\e[0m'
+    red='\e[0;31m'
+    echo ""
+    echo "Searching for ${cyan}$1${NC}"
+    python -c 'import sys; print sys.real_prefix' 2>/dev/null && INVENV=1 || INVENV=0
+    if [ $INVENV -eq 1 ]
+    then
+        cdsitepackages > /dev/null
+        echo ""
+        grep -r "class "$1 | while read -r line ; do
+        line=${line//.py*}
+        line="from "${line//\//.}" import $1"
 
-#### quick backup of file ####
-bak(){
-  cp ./$1 ./$1-bak
-  # touch $1
+        echo $line
+        done
+        echo ""
+  grep -r "def "$1 | while read -r line ; do
+        line=${line//.py*}
+        line="from "${line//\//.}" import $1"
+
+        echo $line
+        done
+        cd - > /dev/null
+
+    else
+        echo ""
+        echo "${red}no virtual env detected${NC}"
+    fi
 }
 
-#### quick restore of file ####
-rest(){
-  cp ./$1-bak ./$1
-  rm ./$1-bak
-}
-
-#### mkdir and navigate to it ####
-mcd () {
-    mkdir -pv $1
-    cd $1
-}
-
-alias mkdir="mkdir -pv" # recursive mkdir
-
-
+##### DEV OPS #####
 alias ps="ps auxf" # process listing table
 alias psg="ps aux | grep -v grep | grep -i -e VSZ -e" # searchable process listing table ($ psg postgres)
 
@@ -65,12 +89,39 @@ alias du="du -ach | sort -h" # moar disk usage
 alias top="htop"  #better formatted top command
 alias histg="history | grep" # display recent history. (helpful to get history id: ie: removing an accidental password entry)
 alias wget="wget -c"  # -c will allow for continuable downloads
+#####
 
+##### Git #####
 alias comall="git commit -am" #trigger git commit all command.
+#####
 
-alias run="python manage.py runserver" # run local django server
+##### Development Folder control #####
+bak(){
+  # quick backup of file
+  cp ./$1 ./$1-bak
+}
 
+rest(){
+  # quick restore of file
+  cp ./$1-bak ./$1
+  rm ./$1-bak
+}
+
+mcd () {
+  # mkdir and navigate to it
+  mkdir -pv $1
+  cd $1
+}
+
+alias mkdir="mkdir -pv" # recursive mkdir
 alias op="nautilus ./"  # open current directory in nautilus
 alias ophome="nautilus ~/"  # open home in nautilus
-alias gotodev="cd ~/Development/Lift" # goto my local dev folder
+alias cddev="cd ~/Development/" # goto my local dev folder
+alias nautdev="nautilus ~/Development/"
+alias cdlift="cd ~/Development/Lift/"
+alias nautdev="nautilus ~/Development/Lift"
+#####
 
+##### Django Dev #####
+alias run="python manage.py runserver" # run local django server
+#####
